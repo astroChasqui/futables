1f927e17f49320ad97143bd4742b3ac30a3248fc
Ivan Ramirez <ircarrascal@gmail.com>
2014-02-25 13:54:14 -0600

    +search games

--- include/database.php
+++ include/database.php
@@ -1,19 +1,19 @@
 <?php
 
-  $db_hostname="localhost";
-  $db_database="futables";
-  $db_username="root";
-  $db_password="jxXp!_9z";
+  //$db_hostname="localhost";
+  //$db_database="futables";
+  //$db_username="root";
+  //$db_password="jxXp!_9z";
 
   //use this to deploy on zymic
   //$db_database="astrochasqui_zxq_futables";
   //$db_username="898078_futables";
   
   //to deploy on 000webhost
-  //$db_hostname = "mysql13.000webhost.com";
-  //$db_database = "a3669356_futable";
-  //$db_username = "a3669356_root";
-  //$db_password = "jxXp!_9z";
+  $db_hostname = "mysql13.000webhost.com";
+  $db_database = "a3669356_futable";
+  $db_username = "a3669356_root";
+  $db_password = "jxXp!_9z";
 
   mysql_connect($db_hostname, $db_username, $db_password)
     or die(mysql_error());
